322cf4b5e6fb74d070c402eb834c75dde68103da
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-08-22 22:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function mincost(arr)
 { 
 //write your code here
-// return the min cost
-  
+
+
+	/**
+	Iterate over the current array DONE
+	find the 2 smallest items - use sort() DONE
+	Add both of them to find the cost to be paid
+	Add this cost to the totalCost
+	Remove the first 2 items, and insert the latest cost/sum we found by connecting the ropes
+	**/
+	let totalCost = 0; 
+	while(arr.length > 1) {
+	arr.sort((a,b) => a-b);
+	let firstSmallest = arr.shift();
+	let secondSmallest = arr.shift();
+
+	let smallestCost =  firstSmallest + secondSmallest;
+	arr.push(smallestCost);
+	totalCost += smallestCost
+	}
+
+	return totalCost;
 }
 
 module.exports=mincost;
